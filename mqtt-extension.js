d219903bd1ae71867d1578cb4abe2e81a81e7779
Kurt Van Dijck <dev.kurt@vandijck-laurijssen.be>
2019-03-23 00:02:55 +0100

    fix indent, whitespace

--- mqtt-extension.js
+++ mqtt-extension.js
@@ -3,88 +3,82 @@
 
 (function(ext) {
 
-  //$.getScript("https://kurt-vd.github.io/scratch-mqtt-extension/jquery.min.js", function(){ console.log("jquery.min.js direct loaded"); });
-  $.getScript("https://kurt-vd.github.io/scratch-mqtt-extension/mqttws31.js", function(){ console.log("mqttws31.js direct loaded"); });
-  console.log( "another log" ); // 200
+	//$.getScript("https://kurt-vd.github.io/scratch-mqtt-extension/jquery.min.js", function(){ console.log("jquery.min.js direct loaded"); });
+	$.getScript("https://kurt-vd.github.io/scratch-mqtt-extension/mqttws31.js", function(){ console.log("mqttws31.js direct loaded"); });
+	console.log( "another log" ); // 200
 
-  var mqtt;
-  var reconnectTimeout = 10000;
-  var msgq = [];
+	var mqtt;
+	var reconnectTimeout = 10000;
+	var msgq = [];
 
-  host = 'server';
-  port = 1883;
-  useTLS = false;
-  username = null;
-  password = null;
-  cleansession = true;
+	host = 'server';
+	port = 1883;
+	useTLS = false;
+	username = null;
+	password = null;
+	cleansession = true;
 
-  console.log("timeout=" + reconnectTimeout);
-
-
-  function MQTTconnect() {
-    console.log("connect to "+ host + ":" + port + " TLS = " + useTLS + " user=" + username + " pwd=" + password);
-
-    mqtt = new Paho.MQTT.Client(
-      host,
-      port,
-      '',
-      "web_" + parseInt(Math.random() * 100, 10)
-    );
-
-    var options = {
-      timeout: 10,
-      useSSL: useTLS,
-      cleanSession: cleansession,
-      onSuccess: onConnect,
-      onFailure: function (message) {
-	      $('#status').val("Connection failed: " + message.errorMessage + "Retrying");
-	      setTimeout(MQTTconnect, reconnectTimeout);
-      }
-    };
-
-    mqtt.onConnectionLost = onConnectionLost;
-    mqtt.onMessageArrived = onMessageArrived;
-
-    if (username != null) {
-        options.userName = username;
-        options.password = password;
-    }
-    console.log("connecting ...");
-    mqtt.connect(options);
-  }
+	console.log("timeout=" + reconnectTimeout);
 
 
-    function onMessageArrived(message) {
-        //console.log("message arrived " + message.payloadString);
-        //messageQueue.push(message);
-	var topic = message.destinationName;
+	function MQTTconnect()
+	{
+		console.log("connect to "+ host + ":" + port + " TLS = " + useTLS + " user=" + username + " pwd=" + password);
 
-	msgq[topic].q.push(message.payloadString);
-    };
+		mqtt = new Paho.MQTT.Client(
+				host,
+				port,
+				'',
+				"web_" + parseInt(Math.random() * 100, 10)
+				);
 
-    function onConnect() {
-        console.log("connected");
-        $('#status').val('Connected to ' + host + ':' + port);
-	// clear object buffers
-	msgq = [];
-    };
+		var options = {
+			timeout: 10,
+			useSSL: useTLS,
+			cleanSession: cleansession,
+			onSuccess: onConnect,
+			onFailure: function (message) {
+				$('#status').val("Connection failed: " + message.errorMessage + "Retrying");
+				setTimeout(MQTTconnect, reconnectTimeout);
+			}
+		};
+
+		mqtt.onConnectionLost = onConnectionLost;
+		mqtt.onMessageArrived = onMessageArrived;
+
+		if (username != null) {
+			options.userName = username;
+			options.password = password;
+		}
+		console.log("connecting ...");
+		mqtt.connect(options);
+	}
+
+	function onConnect()
+	{
+		console.log("connected");
+		$('#status').val('Connected to ' + host + ':' + port);
+		// clear object buffers
+		msgq = [];
+	};
 
 
-    function onConnectionLost(response) {
-        console.log("connection lost");
-        setTimeout(MQTTconnect, reconnectTimeout);
-        $('#status').val("connection lost: " + response.errorMessage + ". Reconnecting");
-    };
+	function onConnectionLost(response)
+	{
+		console.log("connection lost");
+		setTimeout(MQTTconnect, reconnectTimeout);
+		$('#status').val("connection lost: " + response.errorMessage + ". Reconnecting");
+	}
 
+	// Cleanup function when the extension is unloaded
+	ext._shutdown = function() {};
 
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
+	// Status reporting code
+	// Use this to report missing hardware, plugin or unsupported browser
+	ext._getStatus = function()
+	{
+		return {status: 2, msg: 'Ready'};
+	};
 
 	ext.mqtt_connect = function(_host, _port, _tls)
 	{
@@ -93,12 +87,20 @@
 		useTLS = _tls == "true";
 
 		MQTTconnect();
+	};
+
+	function onMessageArrived(message)
+	{
+		var topic = message.destinationName;
+
+		msgq[topic].q.push(message.payloadString);
 	}
 
 	ext.mqtt_recv = function(topic)
 	{
 		return msgq[topic].head;
-	}
+	};
+
 	ext.mqtt_recvd = function(topic)
 	{
 		if (typeof msgq[topic] == 'undefined') {
@@ -113,27 +115,28 @@
 		return false;
 	};
 
-	ext.mqtt_send = function(topic, payload) {
+	ext.mqtt_send = function(topic, payload)
+	{
 		//console.log("publish '" + topic + "'='" + payload + "'");
 		mqtt.send(topic, ''+payload);
 	};
 
 
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            [' ', 'mqtt send %s %n', 'mqtt_send', 'topic', 0],
-            [' ', 'mqtt send %s %s', 'mqtt_send', 'topic', ''],
-            ['r', 'mqtt message %s', 'mqtt_recv', 'topic'],
-            ['h', 'when mqtt %s arrived', 'mqtt_recvd', 'topic'],
+	// Block and block menu descriptions
+	var descriptor = {
+		blocks: [
+			[' ', 'mqtt send %s %n', 'mqtt_send', 'topic', 0],
+			[' ', 'mqtt send %s %s', 'mqtt_send', 'topic', ''],
+			['r', 'mqtt message %s', 'mqtt_recv', 'topic'],
+				['h', 'when mqtt %s arrived', 'mqtt_recvd', 'topic'],
 
-            [' ', 'connect %s : %n tls %m.secureConnection', 'mqtt_connect', 'test.mosquitto.org', 8081, true]
-        ],
-        menus: {
-            secureConnection: ['true', 'false']
-        }
-    };
+				[' ', 'connect %s : %n tls %m.secureConnection', 'mqtt_connect', 'test.mosquitto.org', 8081, true]
+		],
+		menus: {
+			secureConnection: ['true', 'false']
+		}
+	};
 
-    // Register the extension
-    ScratchExtensions.register('MQTT extension', descriptor, ext);
+	// Register the extension
+	ScratchExtensions.register('MQTT extension', descriptor, ext);
 })({});
